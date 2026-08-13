90f255e05986b13d4e049c5220f7a16b5716bf9d
SIBNARAYAN SAMANTA <230678259+sibnarayansamanta7-boop@users.noreply.github.com>
2026-08-13 23:54:17 +0530

    Day 1: Java basics

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("I Like pizza!");
+System.out.println("It's really good!");
+System.out.println("Buy me pizza");
+    }
+}
