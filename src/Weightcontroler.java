dc295f32712305e1045fd5a374eed90fe6480987
SIBNARAYAN SAMANTA <230678259+sibnarayansamanta7-boop@users.noreply.github.com>
2026-08-30 23:05:13 +0530

    Weight machine

---
+++ src/Weightcontroler.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Weightcontroler {
+    public static void main(String[] args){
+        //weight convertion program
+        Scanner anything = new Scanner(System.in);
+
+        // Declare variable
+
+        // welcome message
+
+        // prompt for user choice
+
+        // option 1 convert lbs to kgs
+    }
+}
