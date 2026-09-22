8fb29adcb03c375fbbcd16a4d5182d409b2e70b7
SIBNARAYAN SAMANTA <230678259+sibnarayansamanta7-boop@users.noreply.github.com>
2026-09-22 20:07:31 +0530

    Array

---
+++ src/Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args){
+
+        String[] fruits = {"apple", "orange", "banana", "coconut"};
+
+        fruits[0] ="cherry";
+        int num = fruits.length;
+        System.out.println(fruits[0]);
+        System.out.println(fruits.length);
+    }
+}
