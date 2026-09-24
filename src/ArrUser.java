71ce92244e803bf55b2670acd4c96f9e55c8e32b
SIBNARAYAN SAMANTA <230678259+sibnarayansamanta7-boop@users.noreply.github.com>
2026-09-24 18:11:21 +0530

    Array from users 1

--- src/ArrUser.java
+++ src/ArrUser.java
@@ -1,23 +1,34 @@
 import java.util.Scanner;
 
-public class ArrUser {
-    public static void main(String[] args){
+public class ArrUser  {
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        String[] books ;
+        String[] books;
         int length;
 
-        System.out.println("Enter the number of of element you want in array ");
+        System.out.println("Enter the number of elements you want in array:");
         length = sc.nextInt();
+
+        // Consume the leftover Enter
+        sc.nextLine();
+
         books = new String[length];
 
-        for (int i = 0; i>books.length; i++){
-            System.out.println("Enter one by one ");
+        // Take book names
+        for (int i = 0; i < books.length; i++) {
+            System.out.println("Enter book " + (i + 1) + ":");
             books[i] = sc.nextLine();
         }
-        for (String book : books){
-            System.out.println(books);
+
+        // Print books
+        System.out.println("Your books are:");
+
+        for (String book : books) {
+            System.out.println(book);
         }
+
         sc.close();
     }
 }
